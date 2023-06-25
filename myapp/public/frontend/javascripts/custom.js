ba3fed725c06fc5e396e1ef643ced5b487ed98b4
ttvu <thanhvu6a1@gmail.com>
2023-06-26 00:15:41 +0700

    add fe

---
+++ myapp/public/frontend/javascripts/custom.js
@@ -0,0 +1,125 @@
+$(document).ready(function () {
+    var ckbAll = $(".cbAll");
+    var fmAdmin = $("#zt-form");
+
+    // CKEDITOR
+    if ($('textarea#content_ck').length) {
+        CKEDITOR.replace('content_ck');
+    }
+
+    //call active menu
+    activeMenu();
+
+    //check selectbox
+    change_form_action("#zt-form .slbAction", "#zt-form","#btn-action");
+
+    //check all
+    ckbAll.click(function () {
+        $('input:checkbox').not(this).prop('checked', this.checked);
+        if ($(this).is(':checked')) {
+            $(".ordering").attr("name", "ordering");
+        }else{
+           
+            $(".ordering").removeAttr("name");
+        }
+        
+    });
+    // hiden notify
+    hiddenNotify(".close-btn");
+
+
+
+    $("input[name=cid]").click(function () {
+        if ($(this).is(':checked')) {
+            $(this).parents("tr").find('.ordering').attr("name", "ordering");
+        }else{
+            $(this).parents("tr").find('.ordering').removeAttr("name");
+        }
+    });
+
+    //active menu function
+    function activeMenu() {
+        var arrPathname = window.location.pathname.split('/');
+        var pattern = (typeof arrPathname[2] !== 'undefined') ? arrPathname[2] : '';
+
+        if (pattern != '') {
+            $('#side-menu li a').each(function (index) {
+                var subject = $(this).attr("href");
+                if (subject != "#" && subject.search(pattern) > 0) {
+                    $(this).closest("li").addClass("active");
+                    if ($(this).parents("ul").length > 1) {
+                        $("#side-menu ul").addClass('in').css("height", "auto");
+                        $("#side-menu ul").parent().addClass('active');
+                    }
+                    return;
+                }
+            });
+        } else {
+            $('#side-menu li').first().addClass("active");
+        }
+    }
+
+    //
+    function change_form_action(slb_selector, form_selector, id_btn_action) {
+
+        var optValue;
+        var isDelete = false;
+        var pattenCheckDelete = new RegExp("delete", "i");
+        
+        $(slb_selector).on("change", function () {
+            optValue = $(this).val();
+            
+            
+            if(optValue !== "") {
+                $(id_btn_action).removeAttr('disabled');
+            } else {
+                $(id_btn_action).attr('disabled', 'disabled');
+            }
+            $(form_selector).attr("action", optValue);
+        });
+
+        $(form_selector + " .btnAction").on("click", function () {
+            isDelete = pattenCheckDelete.test($(slb_selector).val());
+            if(isDelete){
+                var confirmDelete = confirm('Are you really want to delete?');
+                if(confirmDelete === false){
+                    return;
+                }
+            }
+
+            var numberOfChecked = $(form_selector + ' input[name="cid"]:checked').length;
+            console.log($(form_selector + ' input[name="cid"]:checked'));
+            if (numberOfChecked == 0) {
+                alert("Please choose some items");
+                return;
+            } else {
+                var flag = false;
+                var str = $(slb_selector + " option:selected").attr('data-comfirm');
+               
+                if (str != undefined) {
+
+                    //Kiểm tra giá trị trả về khi user nhấn nút trên popup
+                    flag = confirm(str);
+                    if (flag == false) {
+                        return flag;
+                    } else {
+                        $(form_selector).submit();
+                    }
+
+                } else {
+                    if (optValue != undefined) {
+                        $(form_selector).submit();
+                    }
+                }
+            }
+
+        });
+    }
+
+    // hidden parent (hidden message notify)
+    function hiddenNotify(close_btn_selector){
+        $(close_btn_selector).on('click', function(){
+            $(this).parent().css({'display':'none'});
+        })    
+    }
+});
---
+++ myapp/public/frontend/javascripts/myjs.js
@@ -0,0 +1,106 @@
+const changeStatus = (id, status) => {
+  let newLinkChangeStatus = `change-status/${id}/${status}`;
+  $.ajax({
+    type: "get",
+    url: newLinkChangeStatus,
+    dataType: "JSON",
+    success: function (response) {
+      // Change status
+      let newStatus = response.newStatus;
+      if (newStatus != undefined && newStatus != "") {
+        let statusClass =
+          newStatus === "active"
+            ? "btn btn-block btn-info"
+            : "btn btn-block btn-danger";
+        $(`#${id}`).html(
+          `<a href="javascript:changeStatus('${id}','${newStatus}')" class="${statusClass}"><span>${newStatus}</span></a>`
+        );
+
+        // Recount status
+        let statusFilter = response.recount;
+        if (statusFilter != undefined) {
+          statusFilter.forEach((filter) => {
+            $(`#${filter.value}`).html(`${filter.name} (${filter.count})`);
+          });
+        }
+        generateNotify("You have changed the status");
+      } else {
+        generateNotify("Failed to change the status");
+      }
+    },
+  });
+};
+
+const changeOrdering = (id, ordering) => {
+  let newLinkChangeStatus = `change-ordering/${id}`;
+  try {
+    $.ajax({
+      type: "post",
+      url: newLinkChangeStatus,
+      data: { ordering: ordering },
+      dataType: "JSON",
+      success: function (response) {},
+    });
+    generateNotify("You have changed the ordering");
+  } catch (error) {
+    generateNotify("Failed to change the ordering");
+  }
+};
+
+const generateNotify = (notify) => {
+  return Toastify({
+    text: notify,
+    duration: 3000,
+  }).showToast();
+};
+
+$("a.btn-delete").on("click", (event) => {
+  event.preventDefault();
+  return Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      const deleteButton = $(event.target);
+      const itemId = deleteButton.data("id");
+      const itemStatus = deleteButton.data("status");
+      $.ajax({
+        type: "DELETE",
+        url: `delete/${itemId}/${itemStatus}`,
+        success: function (response) {
+          Swal.fire("Deleted!", "Your file has been deleted.", "success");
+          deleteButton.closest("tr").remove();
+          // Recount status
+          let statusFilter = response.recount;
+          if (statusFilter != undefined) {
+            statusFilter.forEach((filter) => {
+              $(`#${filter.value}`).html(`${filter.name} (${filter.count})`);
+            });
+          }
+        },
+        error: function (error) {
+          Swal.fire("Error", "Failed to delete the item.", "error");
+        },
+      });
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('.tag-toggle').click(function(e) {
+    e.preventDefault();
+    $(this).siblings('.sub-tags').toggleClass('open');
+  });
+});
+
+function submitCategoryForm() {
+  console.log(123);
+  const form = document.getElementById('categoryForm');
+  const selectedCategoryId = form.filter_category.value;
+  const url = `/admin/article/filter-category/${selectedCategoryId}`;
+  form.action = url;
+  form.submit();
+}
